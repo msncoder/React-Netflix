28b1bb00fb18bb3366a2ac83c7832e3ecd56b677
msncoder <msncoder1999@gmail.com>
2025-05-27 02:01:05 +0500

    add api of movies

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <Router>
       <Header />
-      <Home />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import "./Home.scss";
+
+const Cards = ({ img }) => {
+  return <img src={img} alt="cover" className="card" />;
+};
+
+const Row = ({
+  title,
+  arr = [
+    {
+      img: "https://rukminim2.flixcart.com/image/850/1000/krgohow0/poster/b/h/c/large-riverdale-netflix-series-poster-riverdale-poster-tv-series-original-imag596yhrbbchsz.jpeg?q=90&crop=false",
+    },
+  ],
+}) => {
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div>
+        {arr.map((item, index) => (
+          <Cards key={index} img={item.img} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 function Home() {
   return (
-    <div className="home">
+    <section className="home">
       <div className="banner"></div>
-    </div>
+      <Row title={"popular on Netflix"} />
+      <Row title={"Movies"} />
+      <Row title={"TV Shows"} />
+      <Row title={"Recently Viewed"} />
+      <Row title={"My List"} />
+    </section>
   );
 }
 
